86a3510aa364533b7817ffc26f1e8a2fe391e294
Vikas Jilla <jillavikas@gmail.com>
2020-03-11 08:15:39 +0530

    first commit

---
+++ HTML5AndReactLearning/HTML5Form/alert.js
@@ -0,0 +1,23 @@
+//learning js from javascript.info
+
+//loading external js example
+// alert("I'm from external Javascript")
+
+//using variables
+let admin,name;
+name = 'john';
+//copying variables
+admin = name;
+alert(admin);
+
+let varNumb = '2';
+varNumb = `hi ${varNumb}`;//not single quotes they are back tricks.
+alert(typeof alert);
+
+//naming variables
+let ourPlanetName = 'Earth';
+let currentVisitorName;
+
+// use upper case for only hard-coded constants and lower case for dynamically computed constants
+// const DATE_OF_BIRTH = '18-09-2020';
+// const age = calculateAge(DATE_OF_BIRTH);
